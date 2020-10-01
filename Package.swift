dc56251146d5f107650d69dce8bb71da793c53e0
Hindrik Stegenga <hindrik1997@hotmail.com>
2020-10-01 12:13:41 +0200

    Expose OSCache and OrderedDictionary.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Calendar2",
-            targets: ["Calendar2"]),
+            targets: ["Calendar2", "OSCache", "OrderedDictionary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
